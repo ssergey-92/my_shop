60111788bba0573f361206e2c83fe02ceb416731
Sergey Solop <solop1992@mail.ru>
2024-09-14 21:40:15 +0300

    feat: add try/except for endpoints handlers

--- shop/user_profile/services.py
+++ shop/user_profile/services.py
@@ -2,6 +2,7 @@
 
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -25,7 +26,7 @@ class HandleProfile:
     """Class for handling logic for Profile related endpoints"""
 
     _invalid_password_error = {"error": "Invalid user password!"}
-    _profile_error = {"error": "User profile is not found!"}
+    _profile_error = "User profile is not found!"
     _successful_psw_update = {"msg": "Password was successfully changed."}
     _successful_avatar_update = {"msg": "Avatar was successfully changed."}
     _http_bad_request = HTTP_400_BAD_REQUEST
@@ -40,18 +41,18 @@ class HandleProfile:
         corresponding response.
 
         """
-
-        profile = Profile.get_by_user_id_with_avatar(request.user.id)
-        if profile:
-            try:
+        try:
+            profile = Profile.get_by_user_id_with_avatar(request.user.id)
+            if profile:
                 return Response(
                     OutProfileSerializer(profile).data, cls._http_success,
                 )
-            except AttributeError as exc:
-                app_logger.error(f"Error while serializing Profile: {exc}")
-        else:
-            app_logger.error(cls._profile_error)
-        return Response(cls._profile_error, cls._http_internal_error)
+            exc = cls._profile_error
+        except Exception as exc:
+            pass
+        app_logger.error(exc)
+        return Response({"error": exc}, cls._http_internal_error)
+
 
     @classmethod
     def update_own_avatar(cls, request: Request) -> Response:
@@ -62,17 +63,21 @@ class HandleProfile:
         Return corresponding response.
 
         """
-        validation_error = validate_image_src(request.FILES["avatar"])
-        if validation_error:
-            return Response(validation_error, cls._http_bad_request)
+        try:
+            validation_error = validate_image_src(request.FILES["avatar"])
+            if validation_error:
+                return Response(validation_error, cls._http_bad_request)
 
-        profile = Profile.get_by_user_id_with_avatar(request.user.id)
-        if not profile:
-            app_logger.error(cls._profile_error)
-            Response(cls._profile_error, cls._http_internal_error)
+            profile = Profile.get_by_user_id_with_avatar(request.user.id)
+            if not profile:
+                app_logger.error(cls._profile_error)
+                return Response(cls._profile_error, cls._http_internal_error)
 
-        profile.set_new_avatar(request.FILES["avatar"])
-        return Response(cls._successful_avatar_update, cls._http_success)
+            profile.set_new_avatar(request.FILES["avatar"])
+            return Response(cls._successful_avatar_update, cls._http_success)
+        except Exception as exc:
+            app_logger.error(exc)
+            return Response({"error": exc}, cls._http_internal_error)
 
     @classmethod
     def update_own_profile(cls, request: Request) -> Response:
@@ -83,26 +88,30 @@ class HandleProfile:
         Return corresponding response.
 
         """
-        request.data.pop("avatar")
-        profile_data = InProfileSerializer(data=request.data)
-        if not profile_data.is_valid():
-            return Response(
-                {"error": profile_data.errors}, cls._http_bad_request,
-            )
-
-        profile = Profile.objects.get(user_id=request.user.id)
-        if not profile:
-            Response(cls._profile_error, cls._http_internal_error)
-
         try:
+            request.data.pop("avatar")
+            profile_data = InProfileSerializer(data=request.data)
+            if not profile_data.is_valid():
+                return Response(
+                    {"error": profile_data.errors}, cls._http_bad_request,
+                )
+
+            profile = Profile.objects.get(user_id=request.user.id)
+            if not profile:
+                Response(cls._profile_error, cls._http_internal_error)
+
             profile.full_name = profile_data.validated_data["fullName"]
             profile.unique_email = profile_data.validated_data["email"]
             profile.unique_phone = profile_data.validated_data["phone"]
             profile.save()
+            return Response(
+                OutProfileSerializer(profile).data, cls._http_success,
+            )
         except IntegrityError as exc:
             return Response({"error": exc.args}, cls._http_bad_request)
-
-        return Response(OutProfileSerializer(profile).data, cls._http_success)
+        except Exception as exc:
+            app_logger.error(exc)
+            return Response({"error": exc}, cls._http_internal_error)
 
     @classmethod
     def update_own_password(cls, request: Request) -> Response:
@@ -113,24 +122,29 @@ class HandleProfile:
         Return corresponding response.
 
         """
-        password_details = ChangePasswordSerializer(data=request.data)
-        if not password_details.is_valid():
-            return Response(
-                {"error": password_details.errors}, cls._http_bad_request,
+        try:
+            password_details = ChangePasswordSerializer(data=request.data)
+            if not password_details.is_valid():
+                return Response(
+                    {"error": password_details.errors}, cls._http_bad_request,
+                )
+
+            user = authenticate(
+                username=request.user.username,
+                password=password_details.validated_data["currentPassword"],
             )
-
-        user = authenticate(
-            username=request.user.username,
-            password=password_details.validated_data["currentPassword"],
-        )
-        if not user:
-            return Response(cls._invalid_password_error, cls._http_bad_request)
-
-        update_user_password(
-            user, password_details.validated_data["newPassword"],
-        )
-        cls._reset_user_session(request, user)
-        return Response(cls._successful_psw_update, cls._http_success)
+            if not user:
+                return Response(
+                    cls._invalid_password_error, cls._http_bad_request,
+                )
+            update_user_password(
+                user, password_details.validated_data["newPassword"],
+            )
+            cls._reset_user_session(request, user)
+            return Response(cls._successful_psw_update, cls._http_success)
+        except Exception as exc:
+            app_logger.error(exc)
+            return Response({"error": exc}, cls._http_internal_error)
 
     @staticmethod
     def _reset_user_session(request: Request, user: User) -> None:
