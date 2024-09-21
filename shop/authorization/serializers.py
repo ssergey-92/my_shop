ab4a770ed02cc031e083defb6bbfd9944dd38c31
Sergey Solop <solop1992@mail.ru>
2024-09-21 18:51:12 +0300

    feat: authorization - add extra validation for name of user

--- shop/authorization/serializers.py
+++ shop/authorization/serializers.py
@@ -2,6 +2,7 @@
 
 from rest_framework import serializers
 
+from common.validators import validate_full_name
 
 class SignInSerializer(serializers.Serializer):
     """Class for validating user sign in details."""
@@ -16,3 +17,12 @@ class SignUpSerializer(serializers.Serializer):
     name = serializers.CharField(allow_blank=False, required=True)
     username = serializers.CharField(allow_blank=False, required=True)
     password = serializers.CharField(allow_blank=False, required=True)
+
+    def validate_name(self, name: str) -> str:
+        """Extra validation for field 'name'."""
+
+        validation_error = validate_full_name(name)
+        if not validation_error:
+            return name
+
+        raise serializers.ValidationError(validation_error)
