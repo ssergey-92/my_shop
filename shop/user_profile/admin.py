56852a30d1f76f434e66f749c1ea8cd3b34e2000
Sergey Solop <solop1992@mail.ru>
2024-10-07 12:13:04 +0300

    feat: shop:user_profile add soft delete to custom user admin

--- shop/user_profile/admin.py
+++ shop/user_profile/admin.py
@@ -12,7 +12,7 @@ from django.utils.safestring import SafeString
 
 from .forms import ProfileForm
 from .models import Profile
-
+from common.admin import archive_items, restore_items
 
 class ProfileInline(admin.StackedInline):
     """Class Profile Inline for django admin panel."""
@@ -52,18 +52,22 @@ class ProfileInline(admin.StackedInline):
 class CustomUserAdmin(UserAdmin):
     """Class Custom user admin for build in clas User in django admin panel."""
 
+    model = User
     inlines = [ProfileInline]
-    list_display = ("username", "full_name", "is_superuser", "is_staff")
+    actions = (archive_items, restore_items)
+    list_display = (
+        "id", "username", "full_name", "is_superuser", "is_staff", "is_active",
+    )
     list_display_links = ("username", "full_name")
     list_filter = ("is_active", "is_superuser", "is_staff")
-    ordering = ["pk"]
+    ordering = ("id", "username", "is_superuser", "is_staff")
     fieldsets = (
-        (None, {"fields": ("username", "password")}),
+        ("SOFT DELETE", {"fields": ("is_active",)}),
+        ("DETAILS", {"fields": ("username", "password")}),
         (
             "Permissions",
             {
                 "fields": (
-                    "is_active",
                     "is_staff",
                     "is_superuser",
                     "groups",
@@ -73,21 +77,33 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
-    def get_queryset(self, request: HttpRequest) -> QuerySet:
-        """Get query set with select related objects Profile and Avatar."""
-
-        return (
-            User.objects.select_related("profile")
-            .select_related("profile__avatar")
-            .all()
-        )
-
     def full_name(self, obj: User) -> Optional[str]:
         """Create extra field with user's full name."""
 
         return obj.profile.full_name
 
     full_name.short_description = "FULL NAME"
+    full_name.admin_order_field = "full_name"
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        """Get query set with select related objects Profile and Avatar."""
+
+        qs = super().get_queryset(request)
+        return qs.select_related("profile", "profile__avatar")
+
+
+    def delete_model(self, request: HttpRequest, obj: User) -> None:
+        """Override method. Model instance is archived instead of deletion."""
+
+        obj.is_active = False
+        obj.save()
+
+    def delete_queryset(
+            self, request: HttpRequest, queryset: QuerySet,
+    ) -> None:
+        """Override method. Instances are archived instead of deletion."""
+
+        queryset.update(is_active=False)
 
 
 admin.site.unregister(User)
